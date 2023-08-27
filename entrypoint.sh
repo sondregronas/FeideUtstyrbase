864b44599d93f8db1c0bfb0f57e5a51aa9f2e4bb
Sondre Grønås <44143748+sondregronas@users.noreply.github.com>
2023-08-27 11:16:39 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,5 +15,6 @@ run_app() {
   wait $PID
 }
 
-trap 'kill -TERM $PID && wait $PID && echo "SIGTERM received, exiting." && run_app' TERM HUP INT
+trap 'kill -TERM $PID && wait $PID && echo "SIGTERM received, exiting." && exit 0' TERM
+trap 'kill -TERM $PID && wait $PID && echo "SIGHUP received, restarting." && run_app' HUP
 run_app
