95a890adfb78fbcbc3fefdcb57337c2a31e30a86
Sondre Grønås <44143748+sondregronas@users.noreply.github.com>
2024-07-26 12:00:33 +0200

    Update app.py

--- BookingSystem/app.py
+++ BookingSystem/app.py
@@ -142,8 +142,20 @@ try:
     class App(gunicorn.app.base.BaseApplication):
         def __init__(self):
             start_routine()
+            self.options = {}
             self.application = create_app()
             super().__init__()
+
+
+    def load_config(self):
+        config = {key: value for key, value in self.options.items()
+                  if key in self.cfg.settings and value is not None}
+        for key, value in config.items():
+            self.cfg.set(key.lower(), value)
+
+
+    def load(self):
+        return self.application
 
 
     app = App()
